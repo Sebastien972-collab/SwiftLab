5f64211f7803f34f4fe388f0a05190e2d6824a55
swisslson <cyril.hany@gmail.com>
2025-07-08 16:31:22 +0200

    creation des struct exercice solo

---
+++ SwiftLab/Model/ExercicesSolo/Exercice.swift
@@ -0,0 +1,14 @@
+//
+//  Exercice.swift
+//  SwiftLab
+//
+//  Created by cyrilH on 08/07/2025.
+//
+
+import Foundation
+struct Exercice {
+    var question: String
+    var answers : [String]
+    var goodAnswer: Int
+    var validAnswer: Bool 
+}
---
+++ SwiftLab/Model/ExercicesSolo/ExercicesSolo.swift
@@ -0,0 +1,14 @@
+//
+//  ExercicesSolo.swift
+//  SwiftLab
+//
+//  Created by cyrilH on 08/07/2025.
+//
+
+import Foundation
+struct ExercicesSolo {
+    var nameOfExercices: String
+    var exerciceFinished: Bool
+    var exercice:[Exercice]
+    
+}
