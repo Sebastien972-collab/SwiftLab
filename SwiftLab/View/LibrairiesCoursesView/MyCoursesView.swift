93b963cf87fb4cb8aae641763308765e27d1f715
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-07-09 20:42:27 +0200

    Fix

--- SwiftLab/View/LibrairiesCoursesView/MyCoursesView.swift
+++ SwiftLab/View/LibrairiesCoursesView/MyCoursesView.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct MyCoursesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.background.ignoresSafeArea(edges: .all)
+            VStack(alignment: .leading,content: {
+                CarouselCustomCourse()
+                CardCourseView()
+                Spacer()
+            })
+            .navigationTitle(Text("Mes cours"))
+        }
     }
 }
 
 #Preview {
-    MyCoursesView()
+    NavigationStack {
+        MyCoursesView()
+    }
 }
--- SwiftLab/View/ProgressView/CarouselCustom/CarouselCustomCourse.swift
+++ SwiftLab/View/ProgressView/CarouselCustom/CarouselCustomCourse.swift
@@ -31,6 +31,7 @@ struct CarouselCustomCourse: View {
                     }
                 }
             }
+            .padding(.leading)
         }
     }
 }
