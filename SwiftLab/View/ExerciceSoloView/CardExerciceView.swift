2680763e6da0e7843f5aa284cc69c75c7604b964
swisslson <cyril.hany@gmail.com>
2025-07-09 19:36:57 +0200

    modifiction des quiz de teste plus ajout carte exercice

---
+++ SwiftLab/View/ExerciceSoloView/CardExerciceView.swift
@@ -0,0 +1,18 @@
+//
+//  CardExerciceView.swift
+//  SwiftLab
+//
+//  Created by cyrilH on 09/07/2025.
+//
+
+import SwiftUI
+
+struct CardExerciceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardExerciceView()
+}
