7b9e28f2cd42328006a2c6f52a414dccaa2bd5db
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-07-09 10:50:31 +0200

    Finalisation des vues de connexions, ajouent des vues de connexions avec google, apple facebook non fonctionnel.

---
+++ SwiftLab/View/ConnectionView/SignInView.swift
@@ -0,0 +1,45 @@
+//
+//  SignInView.swift
+//  SwiftLab
+//
+//  Created by Sébastien DAGUIN  on 09/07/2025.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    @Binding var manager: ConnectionManager
+    var body: some View {
+        ZStack {
+            Color.background.ignoresSafeArea(edges: .all)
+            VStack {
+                Spacer()
+                VStack(spacing: 20) {
+                    CustomTextField(placeholder: "Email", text: $manager.username)
+                    CustomSecureField(password: $manager.password)
+                    
+                }
+                Spacer()
+                ContinueButtonView(title: "Se connecter", action: {})
+                Spacer()
+                ExternalConnectionMethodTypeList(selection: .signIn)
+                Spacer()
+                HStack {
+                    Text("Avez-vous un compte ?")
+                    Button {
+                    } label: {
+                        Text("Créer un compte")
+                    }
+
+                }
+                Spacer()
+            }
+            .navigationTitle(Text("Se connecter"))
+        }
+
+    }
+}
+
+#Preview {
+    SignInView(manager: .constant(.init()))
+}
