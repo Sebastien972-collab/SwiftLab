87878cf717f13297e1de958a669358c103c509b5
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-07-09 20:41:46 +0200

    Ajout de la navigation des cartes dans le carouselle

---
+++ SwiftLab/View/LibrairiesCoursesView/MyCoursesView.swift
@@ -0,0 +1,18 @@
+//
+//  MyCoursesView.swift
+//  SwiftLab
+//
+//  Created by Sébastien DAGUIN  on 09/07/2025.
+//
+
+import SwiftUI
+
+struct MyCoursesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCoursesView()
+}
