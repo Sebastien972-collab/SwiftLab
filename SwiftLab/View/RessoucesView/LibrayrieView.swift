7eaa5d9308f17b9510fb4e7795f0307d98758e16
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-07-17 23:31:26 +0200

    Ajout d'une vue LibrairieView et ajout du protocol equatbale dans User

---
+++ SwiftLab/View/RessoucesView/LibrayrieView.swift
@@ -0,0 +1,18 @@
+//
+//  LibrayrieView.swift
+//  SwiftLab
+//
+//  Created by Sébastien DAGUIN  on 17/07/2025.
+//
+
+import SwiftUI
+
+struct LibrayrieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibrayrieView()
+}
