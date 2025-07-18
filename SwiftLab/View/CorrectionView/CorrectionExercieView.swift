b7ff564a44d98c19741d93a6d15f1e8e5ad7c0c2
swisslson <cyril.hany@gmail.com>
2025-07-18 11:32:34 +0200

    finition visuel plus corection exercice

--- SwiftLab/View/CorrectionView/CorrectionExercieView.swift
+++ SwiftLab/View/CorrectionView/CorrectionExercieView.swift
@@ -8,22 +8,72 @@
 import SwiftUI
 
 struct CorrectionExercieView: View {
+    @State var tableau : Array = ["a", "b", "c", "d"]
     var body: some View {
         ZStack{
             Color.customBeige.ignoresSafeArea(.all)
-            VStack{
-                HStack{
-                    VStack{
-                        Text("correction")
-                            .font(.title)
-                            .bold()
-                            .padding(.horizontal)
+            ScrollView(showsIndicators: false){
+                VStack{
+                    HStack{
+                        VStack{
+                            Text("correction")
+                                .font(.title)
+                                .bold()
+                                .padding(.horizontal)
+                        }
+                        Spacer()
                     }
-                    Spacer()
+                    //affichage page suivante ou fin
+                    VStack{
+                        Text("question")
+                            .font(.largeTitle)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .bold()
+                        VStack(spacing : 8){
+                            ForEach(tableau.indices, id: \.self) { choice in
+                                QuizChoiceView( selectedChoice: $tableau[choice] , choices: tableau[choice])
+                                
+                            }
+                        }
+                        ContinueButtonView(title: "Question suivante", color: .customClearOrange) {
+                            withAnimation {
+                                //                            prochaine question -->
+                            }
+                        }
+                        .frame(width: 300, height: 60)
+                        .padding()
+                    }
+                    
+                    VStack{
+                        
+                        VStack{
+                            HStack(alignment: .top){
+                                VStack{
+                                    Image(systemName: "lightbulb.circle.fill")
+                                        .resizable(capInsets: EdgeInsets())
+                                        .frame(width: 24, height: 24)
+                                }.padding(.trailing,5)
+                                
+                                Text("Les constantes sont déclarées en LET tandis que VAR correspond aux variables. Const n’existe pas en Swift et private concerne la portée d’un classe")
+                                    .font(.system(size: 14, weight: .medium))
+                                    .multilineTextAlignment(.leading)
+                                    .foregroundStyle(.black)
+                                    .fixedSize(horizontal: false, vertical: true)
+                            }
+                            
+                        }
+                        
+                        
+                    }
+                    
+                    .padding( 20)
+                    .frame(width: 300 )
+                    .background(Color.grayBorder)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
                 }
-                //affichage page suivante ou fin
+                .padding(.top,40)
             }
-           
         }
         
     }
