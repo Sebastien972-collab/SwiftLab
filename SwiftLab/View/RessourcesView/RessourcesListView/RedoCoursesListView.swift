f6a73616bedc1ad898b28f70f12dd3d7f1fed05f
Thibault Sanclemente <84449794+DevTipsy@users.noreply.github.com>
2025-07-18 13:20:03 +0200

    add folder with files

---
+++ SwiftLab/View/RessourcesView/RessourcesListView/RedoCoursesListView.swift
@@ -0,0 +1,18 @@
+//
+//  RedoCoursesListView.swift
+//  SwiftLab
+//
+//  Created by Thibault on 18/07/2025.
+//
+
+import SwiftUI
+
+struct RedoCoursesListView: View {
+    var body: some View {
+        Text("RedoCoursesListView")
+    }
+}
+
+#Preview {
+    RedoCoursesListView()
+}
---
+++ SwiftLab/View/RessourcesView/RessourcesListView/RedoExercisesListView.swift
@@ -0,0 +1,18 @@
+//
+//  RedoExercisesListView.swift
+//  SwiftLab
+//
+//  Created by Thibault on 18/07/2025.
+//
+
+import SwiftUI
+
+struct RedoExercisesListView: View {
+    var body: some View {
+        Text("RedoExercisesListView")
+    }
+}
+
+#Preview {
+    RedoExercisesListView()
+}
