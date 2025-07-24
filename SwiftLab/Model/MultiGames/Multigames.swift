b664ffcae0e8662a817c6d121f09cf91bb0c5a9c
swisslson <cyril.hany@gmail.com>
2025-07-24 15:03:40 +0200

    model

---
+++ SwiftLab/Model/MultiGames/Multigames.swift
@@ -0,0 +1,19 @@
+//
+//  Multigames.swift
+//  SwiftLab
+//
+//  Created by cyrilH on 24/07/2025.
+//
+
+import Foundation
+
+struct Multigames {
+    var names : String
+    var difficulter : Int
+    var description : String
+    var goodAnswer : String
+    var group : [User]
+    var image : URL
+    
+    
+}
